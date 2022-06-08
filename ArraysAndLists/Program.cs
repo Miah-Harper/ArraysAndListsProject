b34de90f9f5ea1fdc4edd3332f2558647ed58e38
Miah Harper <harpermiah@yahoo.com>
2022-06-08 15:24:49 -0400

    created arrays and lists

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,8 +10,30 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            var myArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evens = new List<int>();
+            var odds = new List<int>();
 
+            //for (int i = 2; i <= 10; i +=2)
+            //{
+            // evens.Add(i);
+            //}
+
+            foreach (var num in myArray)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+
+                else
+                {
+
+                    odds.Add(num);
+                }
+            }
             /* Create two Lists of type int.
+            
              * Name one List "evens"
              * Name the other List "odds"
              */
@@ -27,6 +50,19 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
+            foreach (var num in evens)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine();
+
+            foreach (var num in odds)
+
+            {
+                Console.WriteLine(num);
+            }
         }
     }
+
 }
